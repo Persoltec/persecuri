f5789d010598ea4808a8165b4b91dad5ede7c70c
Persoltec <persoltec.com@gmail.com>
2020-02-24 18:40:44 -0500

    ini

---
+++ seguridad/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "theme-ui";
+
+export default () => {
+  return (
+    <>
+      <Button mr={2}>locasgfdgdfooo</Button>
+      <Button variant="secondary">Boop</Button>
+      <div>Homepage in a user's site</div>
+    </>
+  );
+};
